51c5cd4c1f4c56ea7d5910b408d232fad8f902a5
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-11-06 22:27:02 +0530

    homeBanner

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { useEffect } from "react";
 import { fetchDatafromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PagesNotFound from "./pages/404/pageNotFound";
 
 const App = () => {
   const dispatch = useDispatch();
-  const {url} = useSelector((state) => state.home);
+  const { url } = useSelector((state) => state.home);
 
   useEffect(() => {
     apiTesting();
@@ -17,10 +26,19 @@ const App = () => {
       dispatch(getApiConfiguration(res));
     });
   };
+
   return (
-    <div className="App">
-      {url?.total_pages}
-    </div>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PagesNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.scss";
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (e) =>{
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`)
+    } 
+  }
+
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to dsicover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input onKeyUp={searchQueryHandler} onChange={(e)=> setQuery(e.target.value)} type="text" placeholder="Search for movie or tv show..." />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;

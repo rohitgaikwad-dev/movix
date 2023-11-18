211c0dd67cfb95ee9f91ebbf31f96924b1ff5907
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-11-18 23:23:41 +0530

    details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,16 +1,19 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
-import "./style.scss"
+import "./style.scss";
 
-import useFetch from "../../hooks/useFetch"
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 const Details = () => {
   // const { mediaType, id }= useParams();
   // const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
   return (
-    <div>Details</div>
-  )
-}
+    <div>
+      <DetailsBanner />
+    </div>
+  );
+};
 
-export default Details
+export default Details;
--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -13,6 +13,13 @@ import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
 
 const DetailsBanner = ({ video, crew }) => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}`);
+
+  const { url } = useSelector((state) => state.home);
+
+  const _genres = data?.genres?.map((g) => g.id);
+
   const toHoursAndMinutes = (totalMinutes) => {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
@@ -22,7 +29,42 @@ const DetailsBanner = ({ video, crew }) => {
   return (
     <div className="detailsBanner">
       {!loading ? (
-        <div>Details Content...</div>
+        <>
+          {!!data && (
+            <>
+              <div className="backdrop-img">
+                <Img src={url.backdrop + data.backdrop_path} />
+              </div>
+              <div className="opacity-layer"></div>
+              <ContentWrapper>
+                <div className="left">
+                  {data.poster_path ? (
+                    <Img
+                      className="posterImg"
+                      src={url.backdrop + data.poster_path}
+                    />
+                  ) : (
+                    <Img className="posterImg" src={PosterFallback} />
+                  )}
+                </div>
+                <div className="right">
+                  <div className="title">
+                    {`${data.name || data.title} (${dayjs(
+                      data?.release_date
+                    ).format("YYYY")})`}
+                  </div>
+                  <div className="subtitle">{data.tagline}</div>
+
+                  <Genres data={_genres} />
+
+                  <div className="row">
+                    <CircleRating rating={data.vote_average.toFixed(1)} />
+                  </div>
+                </div>
+              </ContentWrapper>
+            </>
+          )}
+        </>
       ) : (
         <div className="detailsBannerSkeleton">
           <ContentWrapper>

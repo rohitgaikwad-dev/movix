c612f0f5a761b6f4a2eceeec29ba5a7ffe958709
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-12-08 22:20:58 +0530

    explore page

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -22,10 +22,10 @@ const MovieCard = ({ data, fromSearch, mediaType }) => {
       <div className="posterBlock">
         <Img className="posterImg" src={posterUrl} />
         {!fromSearch && (
-          <React.Fragment>
+          <>
             <CircleRating rating={data.vote_average.toFixed(1)} />
             <Genres data={data.genre_ids.slice(0, 2)} />
-          </React.Fragment>
+          </>
         )}
       </div>
       <div className="textBlock">
